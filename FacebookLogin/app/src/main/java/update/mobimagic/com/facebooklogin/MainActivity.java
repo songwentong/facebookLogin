16f6a4d6a94c4cb808326fcf337b0e45605fef7c
songwentong <songwentong@mobimagic.com>
2017-09-22 19:20:03 +0800

    获取登陆信息

--- FacebookLogin/app/src/main/java/update/mobimagic/com/facebooklogin/MainActivity.java
+++ FacebookLogin/app/src/main/java/update/mobimagic/com/facebooklogin/MainActivity.java
@@ -1,16 +1,35 @@
 package update.mobimagic.com.facebooklogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +41,89 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AccessToken token = AccessToken.getCurrentAccessToken();
+                if (token != null){
+                    tokenGetEmail(token);
+                }
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        //loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+            }
+        });
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken token = AccessToken.getCurrentAccessToken();
+                //Profile profile = Profile.getCurrentProfile();
+                tokenGetEmail(token);
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
     }
+
+    void tokenGetEmail(AccessToken token){
+        GraphRequest request = GraphRequest.newMeRequest( token,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        Log.v("LoginActivity Response ", response.toString());
+
+                        try {
+                            String Name = object.getString("name");
+
+                            String FEmail = object.getString("email");
+                            Log.v("Email = ", " " + FEmail);
+                            Toast.makeText(getApplicationContext(), "Name " + Name, Toast.LENGTH_LONG).show();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,gender, birthday");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        AccessToken token = AccessToken.getCurrentAccessToken();
+
+        Profile profile = Profile.getCurrentProfile();
+
+        Log.i("","token:"+token+" profile:"+profile);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
